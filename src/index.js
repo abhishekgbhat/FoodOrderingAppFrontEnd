2c4091fa9010d252b2a710f0444f410472c4b1a5
Abhishek G Bhat <abhishek.bhat5@gmail.com>
2019-07-06 08:52:36 +0530

    Common headers and initial changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import 'typeface-roboto';
+import App from "./screens/App";
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';  
+
+const theme = createMuiTheme({
+    palette: {
+       primary: {
+          light: '#fff',
+          main: "#3f51b5",
+          dark: '#000'
+       }
+    },
+    typography: { 
+       useNextVariants: true
+    }
+ });
 
 ReactDOM.render(
-    <div>
-        Food Ordering App
-    </div>,
+     <MuiThemeProvider theme = { theme }>
+      <App />
+    </MuiThemeProvider>,
     document.getElementById('root')
 );
 
