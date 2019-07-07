b44e6693db63ba67611b088a42841e2be6548283
schandrakandi <shoukathali.chandrakandi@riverbed.com>
2019-07-07 14:23:16 +0530

    Update Controller.js

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,41 +1,26 @@
-import React, { Component } from 'react';
-import Home from './home/Home';
-import Details from './details/Details';
-import Checkout from './checkout/Checkout';
-import Profile from './profile/Profile';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../screens/home/Home";
+import Profile from "../screens/profile/Profile";
+import Details from "../screens/details/Details";
+import Checkout from "../screens/checkout/Checkout";
+
 
 class Controller extends Component {
 
     constructor() {
         super();
-        this.baseUrl = 'http://localhost:8080/api/';
+        this.baseUrl = "http://localhost:8080/api";
     }
 
     render() {
-        return (
+        return(
             <Router>
-                <div className='main-container'>
-
-                    {/* route to home page */}
-                    <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
-
-                    {/* route to details page */}
-                    <Route path='/restaurant/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
-
-                    {/* route to checkout page,
-                        redirect to home page if a customer tries to go to the checkout page directly
-                    */}
-                    <Route path='/checkout' render={(props) => (
-                        sessionStorage.getItem('customer-cart') === null ? (
-                            <Redirect to='/' />
-                        ) : (
-                                <Checkout {...props} baseUrl={this.baseUrl} />
-                            )
-                    )} />
-
-                    {/* route to profile page */}
-                    <Route path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
+                <div className="main-container">
+                  <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
+                  <Route path='/restaurant/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
+                  <Route path='/Checkout' render={(props) => <Checkout {...props} baseUrl={this.baseUrl} />} />
+                  <Route path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
                 </div>
             </Router>
         )
